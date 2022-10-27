26ada51db0616086ecbf34bba2034a347b2f4b8a
Igor Lopes <claudioigorai@gmail.com>
2022-10-27 11:52:39 -0300

    Estrutura Inicial

---
+++ src/main/java/one/digital/innovation/cloudparking/CloudParkingApplication.java
@@ -0,0 +1,14 @@
+package one.digital.innovation.cloudparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class CloudParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParkingApplication.class, args);
+	}
+
+}
